2d0d0e7250d06c1f9d69951ae3dc61b0612e60d2
Zsolti <zsolt.borcsok@gmail.com>
2014-02-15 12:20:29 +0100

    Maven project + code formatting

--- Bolts/src/bolts/Capture.java
+++ Bolts/src/bolts/Capture.java
@@ -1,11 +1,7 @@
 /*
- *  Copyright (c) 2014, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * Copyright (c) 2014, Facebook, Inc. All rights reserved. This source code is licensed under the BSD-style license
+ * found in the LICENSE file in the root directory of this source tree. An additional grant of patent rights can be
+ * found in the PATENTS file in the same directory.
  */
 package bolts;
 
@@ -15,20 +11,20 @@ package bolts;
  * @param <T>
  */
 public class Capture<T> {
-  private T value;
+    private T value;
 
-  public Capture() {
-  }
+    public Capture() {
+    }
 
-  public Capture(T value) {
-    this.value = value;
-  }
+    public Capture(T value) {
+        this.value = value;
+    }
 
-  public T get() {
-    return value;
-  }
+    public T get() {
+        return value;
+    }
 
-  public void set(T value) {
-    this.value = value;
-  }
+    public void set(T value) {
+        this.value = value;
+    }
 }
