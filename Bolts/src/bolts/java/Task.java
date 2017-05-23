2bf1a4ed25d637fe6c84f496ca7410ee10b78e4b
Zsolti <zsolt.borcsok@gmail.com>
2017-05-23 21:09:08 +0200

    Make Bolts-Java to be Java 8 compatible.

--- Bolts/src/bolts/java/Task.java
+++ Bolts/src/bolts/java/Task.java
@@ -88,7 +88,7 @@ public class Task<TResult> {
      * Creates a completed task with the given value.
      */
     public static <TResult> Task<TResult> forResult(TResult value) {
-        Task<TResult>.TaskCompletionSource tcs = Task.create();
+        Task<TResult>.TaskCompletionSource tcs = Task.<TResult> create();
         tcs.setResult(value);
         return tcs.getTask();
     }
@@ -97,7 +97,7 @@ public class Task<TResult> {
      * Creates a faulted task with the given error.
      */
     public static <TResult> Task<TResult> forError(Exception error) {
-        Task<TResult>.TaskCompletionSource tcs = Task.create();
+        Task<TResult>.TaskCompletionSource tcs = Task.<TResult> create();
         tcs.setError(error);
         return tcs.getTask();
     }
@@ -106,7 +106,7 @@ public class Task<TResult> {
      * Creates a cancelled task with the given error.
      */
     public static <TResult> Task<TResult> cancelled() {
-        Task<TResult>.TaskCompletionSource tcs = Task.create();
+        Task<TResult>.TaskCompletionSource tcs = Task.<TResult> create();
         tcs.setCancelled();
         return tcs.getTask();
     }
@@ -142,7 +142,7 @@ public class Task<TResult> {
      * Invokes the callable on the current thread, producing a Task.
      */
     public static <TResult> Task<TResult> call(final Callable<TResult> callable) {
-        final Task<TResult>.TaskCompletionSource tcs = Task.create();
+        final Task<TResult>.TaskCompletionSource tcs = Task.<TResult> create();
         try {
             tcs.setResult(callable.call());
         } catch (Exception e) {
@@ -159,7 +159,7 @@ public class Task<TResult> {
             return Task.forResult(null);
         }
 
-        final Task<Void>.TaskCompletionSource allFinished = Task.create();
+        final Task<Void>.TaskCompletionSource allFinished = Task.<Void> create();
         final ArrayList<Exception> errors = new ArrayList<Exception>();
         final Object errorLock = new Object();
         final Integer[] count = new Integer[] { tasks.size() };
@@ -228,7 +228,7 @@ public class Task<TResult> {
     public <TContinuationResult> Task<TContinuationResult> continueWith(
             final Continuation<TResult, TContinuationResult> continuation) {
         boolean completed = false;
-        final Task<TContinuationResult>.TaskCompletionSource tcs = Task.create();
+        final Task<TContinuationResult>.TaskCompletionSource tcs = Task.<TContinuationResult> create();
         synchronized (lock) {
             completed = this.isCompleted();
             if (!completed) {
@@ -263,20 +263,20 @@ public class Task<TResult> {
     public <TContinuationResult> Task<TContinuationResult> continueWithTask(
             final Continuation<TResult, Task<TContinuationResult>> continuation) {
         boolean completed = false;
-        final Task<TContinuationResult>.TaskCompletionSource tcs = Task.create();
+        final Task<TContinuationResult>.TaskCompletionSource tcs = Task.<TContinuationResult> create();
         synchronized (lock) {
             completed = this.isCompleted();
             if (!completed) {
                 this.continuations.add(new Continuation<TResult, Void>() {
                     public Void then(Task<TResult> task) {
-                        completeAfterTask(tcs, continuation, task);
+                        Task.<TContinuationResult, TResult> completeAfterTask(tcs, continuation, task);
                         return null;
                     }
                 });
             }
         }
         if (completed) {
-            completeAfterTask(tcs, continuation, this);
+            Task.<TContinuationResult, TResult> completeAfterTask(tcs, continuation, this);
         }
         return tcs.getTask();
     }
